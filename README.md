1145dc62dfe8ee2bc1b904bf45a9c3c8f9d3ace5
nadirdeveloper <nadirali.41006@gmail.com>
2021-09-05 05:18:40 +0500

    fixed email issue

--- README.md
+++ README.md
@@ -10,22 +10,4 @@ For Development:
 
 For Production:
 
-'http://localhost:5000'
-
-## Open Endpoints
-
-Open endpoints require no Authentication.
-
-* [Login](documentation/login.md) : `POST /auth/login`
-
-Open endpoints require no Authentication.
-
-* [Registration](documentation/signup.md) : `POST /auth/signup`
-
-
-## Endpoints that require Authentication
-
-Closed endpoints require a valid Token to be included in the header of the
-request. A Token can be acquired from the Login view above.
-
-* [Get Parking Areas](documentation/getParkingAreas.md) : `POST /parking/getParkingAreas`
+'https://parking-web-server.herokuapp.com'
--- controllers/ParkingController.js
+++ controllers/ParkingController.js
@@ -1,90 +1,83 @@
 const { GenerateUniqueId } = require("../helpers/uniqueId");
 var nodemailer = require('nodemailer');
 
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.SENDER_EMAIL,
-        pass: process.env.SENDER_PASSWORD
-    }
-});
 module.exports = function (app, mongoose) {
-    const { Area, Parking, Booking, Feedback,User } = app.db.models;
-    const GetParkingAreas = async (req, res) => {
-        try {
-            const allAreas = await Area.find();
-            res.json({ success: true, allAreas, message: "Successfully Found All Areas" });
-        } catch (error) {
-            if (error) {
-                res.json({ success: false, error: error.message })
-            }
-        }
+  const { Area, Parking, Booking, Feedback, User } = app.db.models;
+  const GetParkingAreas = async (req, res) => {
+    try {
+      const allAreas = await Area.find();
+      res.json({ success: true, allAreas, message: "Successfully Found All Areas" });
+    } catch (error) {
+      if (error) {
+        res.json({ success: false, error: error.message })
+      }
     }
-    const GetParkingSlots = async (req, res) => {
-        try {
-            const { areaId, selectedStartTime, selectedEndTime, selectedDate } = req.body;
-            const dataToCheck = {
-                areaId,
-                startTime: new Date(`${selectedDate} ${selectedStartTime}`).getTime(),
-                endTime: new Date(`${selectedDate} ${selectedEndTime}`).getTime(),
-            }
-            const allParkings = await Parking.find({ areaId });
-            let gotParkings = [];
-            for (let i = 0; i < allParkings.length; i++) {
-                let eachParking = allParkings[i];
-                const checkUnavailable = await Booking.find({ startTime: { $gte: dataToCheck.startTime }, endTime: { $lte: dataToCheck.endTime }, areaId, parkingId: eachParking.id, isBooking: true })
-                if (checkUnavailable.length != 0) {
-                    let newParking = { ...eachParking._doc, isAvailable: false }
-                    gotParkings.push(newParking)
-                } else {
-                    let newParking = { ...eachParking._doc, isAvailable: true }
-                    gotParkings.push(newParking)
-                }
-            }
-            res.json({ success: true, allParkings: gotParkings, message: "Successfully found All Parkings" })
-        } catch (error) {
-            if (error) {
-                res.json({ success: false, message: error.message });
-            }
+  }
+  const GetParkingSlots = async (req, res) => {
+    try {
+      const { areaId, selectedStartTime, selectedEndTime, selectedDate } = req.body;
+      const dataToCheck = {
+        areaId,
+        startTime: new Date(`${selectedDate} ${selectedStartTime}`).getTime(),
+        endTime: new Date(`${selectedDate} ${selectedEndTime}`).getTime(),
+      }
+      const allParkings = await Parking.find({ areaId });
+      let gotParkings = [];
+      for (let i = 0; i < allParkings.length; i++) {
+        let eachParking = allParkings[i];
+        const checkUnavailable = await Booking.find({ startTime: { $gte: dataToCheck.startTime }, endTime: { $lte: dataToCheck.endTime }, areaId, parkingId: eachParking.id, isBooking: true })
+        if (checkUnavailable.length != 0) {
+          let newParking = { ...eachParking._doc, isAvailable: false }
+          gotParkings.push(newParking)
+        } else {
+          let newParking = { ...eachParking._doc, isAvailable: true }
+          gotParkings.push(newParking)
         }
+      }
+      res.json({ success: true, allParkings: gotParkings, message: "Successfully found All Parkings" })
+    } catch (error) {
+      if (error) {
+        res.json({ success: false, message: error.message });
+      }
     }
+  }
 
-    const BookParkingSlot = async (req, res) => {
-        try {
-            const { areaId, selectedStartTime, selectedEndTime, selectedDate, parkingId } = req.body;
-            const bookingId = mongoose.Types.ObjectId();
-            const dataToSave = {
-                _id: bookingId,
-                id: bookingId,
-                areaId,
-                parkingId,
-                userId: req.user.user,
-                startTime: new Date(`${selectedDate} ${selectedStartTime}`).getTime(),
-                endTime: new Date(`${selectedDate} ${selectedEndTime}`).getTime(),
-                isBooking: true
-            }
-            const newBooking = new Booking(dataToSave);
-            let saveBooking = await newBooking.save();
-            sendConfirmationEmail(req.user.email, bookingId, dataToSave.startTime, dataToSave.endTime);
-            res.json({ success: true, message: "Successfully Saved Booking" })
-        } catch (error) {
-            console.log(error)
-            if (error) {
-                res.json({ success: false, message: error.message });
-            }
-        }
+  const BookParkingSlot = async (req, res) => {
+    try {
+      const { areaId, selectedStartTime, selectedEndTime, selectedDate, parkingId } = req.body;
+      const bookingId = mongoose.Types.ObjectId();
+      const dataToSave = {
+        _id: bookingId,
+        id: bookingId,
+        areaId,
+        parkingId,
+        userId: req.user.user,
+        startTime: new Date(`${selectedDate} ${selectedStartTime}`).getTime(),
+        endTime: new Date(`${selectedDate} ${selectedEndTime}`).getTime(),
+        isBooking: true
+      }
+      const newBooking = new Booking(dataToSave);
+      let saveBooking = await newBooking.save();
+      sendConfirmationEmail(req.user.email, bookingId, dataToSave.startTime, dataToSave.endTime);
+      res.json({ success: true, message: "Successfully Saved Booking" })
+    } catch (error) {
+      console.log(error)
+      if (error) {
+        res.json({ success: false, message: error.message });
+      }
     }
+  }
 
-    const sendConfirmationEmail = (reciever, bookingId, startTime, endTime) => {
-        Booking.findOne({ id: bookingId }).populate({ path: 'parkingId', model: Parking }).populate({ path: 'areaId', model: Area }).then((data) => {
-            const parkingArea = data.areaId.name;
-            const slotName = data.parkingId.name;
-            User.findOne({ email: reciever }).then((user) => {
-                const mailOptions = {
-                    from: 'PARKING TEAM <daveed42401@gmail.com>', // sender address
-                    to: [reciever], // list of receivers
-                    subject: 'Parking Booking Confirmation', // Subject line
-                    html: `<html xmlns="http://www.w3.org/1999/xhtml">
+  const sendConfirmationEmail = (reciever, bookingId, startTime, endTime) => {
+    Booking.findOne({ id: bookingId }).populate({ path: 'parkingId', model: Parking }).populate({ path: 'areaId', model: Area }).then((data) => {
+      const parkingArea = data.areaId.name;
+      const slotName = data.parkingId.name;
+      User.findOne({ email: reciever }).then((user) => {
+        const mailOptions = {
+          from: 'PARKING TEAM <daveed42401@gmail.com>', // sender address
+          to: [reciever], // list of receivers
+          subject: 'Parking Booking Confirmation', // Subject line
+          html: `<html xmlns="http://www.w3.org/1999/xhtml">
                 <head>
                 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
                 <title>Water Montoring cron job</title>
@@ -252,103 +245,111 @@ module.exports = function (app, mongoose) {
                 </tr>
                 </table>
                 </body>`
-                };
-                transporter.sendMail(mailOptions, function (err, info) {
-                    if (err)
-                        console.log(err)
-                    else {
-                        console.log(info);
-                        console.log("Successfully sent email")
-                    }
-                })
-            }).catch((err) => {
-                if (err) {
-                    console.log(err)
-                }
-            })
+        };
 
-        }).catch((err) => {
-            if (err) {
-                console.log(err);
-            }
+        var transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            user: process.env.SENDER_EMAIL,
+            pass: process.env.SENDER_PASSWORD
+          }
+        });
+        transporter.sendMail(mailOptions, function (err, info) {
+          if (err)
+            console.log(err)
+          else {
+            console.log(info);
+            console.log("Successfully sent email")
+          }
         })
-
-    }
-
-    const GetUserBookings = async (req, res) => {
-        try {
-            const userBookings = await Booking.find({ userId: req.user.user }).populate({
-                path: 'areaId',
-                model: Area,
-                select: 'name'
-            }).populate({
-                path: 'parkingId',
-                model: Parking,
-                select: 'name'
-            });
-            res.json({ success: true, message: "Successfully Found Bookings", userBookings })
-        } catch (error) {
-            if (error) {
-                res.json({ success: false, error: error.message })
-            }
+      }).catch((err) => {
+        if (err) {
+          console.log(err)
         }
-    }
+      })
 
-    const SaveFeedbackController = async (req, res) => {
-        try {
-            const { name, feedback } = req.body;
-            const feedbackId = mongoose.Types.ObjectId();
-            const dataToSave = {
-                _id: feedbackId,
-                id: feedbackId,
-                email: req.user.email,
-                userId: req.user.user,
-                fullName: name,
-                message: feedback,
-                reply: false
-            }
-            const newFeedback = new Feedback(dataToSave);
-            const saveFeedback = newFeedback.save();
-            res.json({ success: true, message: "Successfully Saved Your Feedback" })
-        } catch (error) {
-            if (error) {
-                res.json({ success: false, error: error.message })
-            }
-        }
-    }
+    }).catch((err) => {
+      if (err) {
+        console.log(err);
+      }
+    })
 
-    const ViewFeedbackController = async (req, res) => {
-        try {
-            const allUserFeedbacks = await Feedback.find({ userId: req.user.user });
-            res.json({ success: true, message: "Successfully Found All Feedbacks", allUserFeedbacks })
-        } catch (error) {
-            if (error) {
-                res.json({ success: false, error: error.message })
-            }
-        }
-    }
+  }
 
-    const CancelBookingController = async (req, res) => {
-        try {
-            const { bookingId } = req.body;
-            let deleteBooking = await Booking.updateOne({ _id: bookingId }, { isBooking: false });
-            if (deleteBooking.deletedCount === 1) {
-                res.json({ success: true, message: 'Successfully Cancelled Your Booking' })
-            } else {
-                res.json({ success: true, message: 'Sorry No User Found' })
-            }
-        } catch (error) {
-            if (error) {
-                res.json({ success: false, message: error.message })
-            }
-        }
+  const GetUserBookings = async (req, res) => {
+    try {
+      const userBookings = await Booking.find({ userId: req.user.user }).populate({
+        path: 'areaId',
+        model: Area,
+        select: 'name'
+      }).populate({
+        path: 'parkingId',
+        model: Parking,
+        select: 'name'
+      });
+      res.json({ success: true, message: "Successfully Found Bookings", userBookings })
+    } catch (error) {
+      if (error) {
+        res.json({ success: false, error: error.message })
+      }
     }
+  }
 
-    app.controllers.CancelBookingController = CancelBookingController;
-    app.controllers.GetParkingAreas = GetParkingAreas;
-    app.controllers.GetUserBookings = GetUserBookings;
-    app.controllers.SaveFeedbackController = SaveFeedbackController;
-    app.controllers.ViewFeedbackController = ViewFeedbackController;
-    app.controllers.GetParkingSlots = GetParkingSlots;
-    app.controllers.BookParkingSlot = BookParkingSlot;
+  const SaveFeedbackController = async (req, res) => {
+    try {
+      const { name, feedback } = req.body;
+      const feedbackId = mongoose.Types.ObjectId();
+      const dataToSave = {
+        _id: feedbackId,
+        id: feedbackId,
+        email: req.user.email,
+        userId: req.user.user,
+        fullName: name,
+        message: feedback,
+        reply: false
+      }
+      const newFeedback = new Feedback(dataToSave);
+      const saveFeedback = newFeedback.save();
+      res.json({ success: true, message: "Successfully Saved Your Feedback" })
+    } catch (error) {
+      if (error) {
+        res.json({ success: false, error: error.message })
+      }
+    }
+  }
+
+  const ViewFeedbackController = async (req, res) => {
+    try {
+      const allUserFeedbacks = await Feedback.find({ userId: req.user.user });
+      res.json({ success: true, message: "Successfully Found All Feedbacks", allUserFeedbacks })
+    } catch (error) {
+      if (error) {
+        res.json({ success: false, error: error.message })
+      }
+    }
+  }
+
+  const CancelBookingController = async (req, res) => {
+    try {
+      const { bookingId } = req.body;
+      let deleteBooking = await Booking.updateOne({ _id: bookingId }, { isBooking: false });
+      if (deleteBooking.deletedCount === 1) {
+        res.json({ success: true, message: 'Successfully Cancelled Your Booking' })
+      } else {
+        res.json({ success: true, message: 'Sorry No User Found' })
+      }
+    } catch (error) {
+      if (error) {
+        res.json({ success: false, message: error.message })
+      }
+    }
+  }
+
+  app.controllers.CancelBookingController = CancelBookingController;
+  app.controllers.GetParkingAreas = GetParkingAreas;
+  app.controllers.GetUserBookings = GetUserBookings;
+  app.controllers.SaveFeedbackController = SaveFeedbackController;
+  app.controllers.ViewFeedbackController = ViewFeedbackController;
+  app.controllers.GetParkingSlots = GetParkingSlots;
+  app.controllers.BookParkingSlot = BookParkingSlot;
 }
